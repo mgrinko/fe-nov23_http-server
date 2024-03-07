0a711cae8ebd8cc8af49d3b6f37e09cc9d655b04
Misha Grinko <glukym@gmail.com>
2024-03-07 10:15:28 +0200

    add simple server

---
+++ index.js
@@ -0,0 +1,10 @@
+// create simple http server
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.end('Hello World');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://127.0.0.1:${port}`);
+});
